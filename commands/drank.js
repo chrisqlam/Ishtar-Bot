371d0d6ad849ff4d243095d6ad1afbfd31484197
Chris <chrisqlam@gmail.com>
2021-03-02 11:09:48 -0700

    Update drank.js
    No longer double mentions.

--- commands/drank.js
+++ commands/drank.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'drank',
 	description: 'You drank? GOOD JOB!!',
 	execute(message) {
-		message.reply(`${message.author.username}, Nice job!`);
+		message.reply(`Nice job!`);
 	},
 };
